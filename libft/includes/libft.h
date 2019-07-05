1062d7b4cdb94b9a27034b3ca7ae1704697cb61b
qvdp <vanderperre.quentin@gmail.com>
2019-07-05 17:20:35 +0200

    fix issue with libft

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -84,8 +84,6 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				*ft_memalloc(size_t size);
 void				*ft_memset(void *b, int c, size_t len);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
-void				*ft_memccpy(void *dst, const void *src, int c,
-		size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
 void				ft_memdel(void **ap);
